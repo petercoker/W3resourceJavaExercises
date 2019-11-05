4fbfd16f817ee88cfc9e0fecdd23ff663a0a4311
peter <petercoker@protonmail.com>
2019-11-05 02:58:07 +0000

    complete ex6

--- src/com/w3resource/BasicExercisesPart_1/Ex5.java
+++ src/com/w3resource/BasicExercisesPart_1/Ex5.java
@@ -1,10 +1,24 @@
 package com.w3resource.BasicExercisesPart_1;
+import java.util.Scanner;
 
 public class Ex5 {
 
-    public int Multiple(int num1, int num2){
+    public double Multiple(double num1, double num2){
         return num1 * num2;
     }
+	
+	public void Multiple(){
+        Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+		
+		int num1 = input.nextInt();
+        int num2 = input2.nextInt();
+        int sum = num1 * num2;
+		
+		System.out.print("" + num1 + " x" + " " + num2 + " =" + " " + sum);
+    }
+	
+	   
 
 
 }
--- src/com/w3resource/Main.java
+++ src/com/w3resource/Main.java
@@ -1,22 +1,14 @@
 package com.w3resource;
 
-// import com.w3resource.BasicExercisesPart_1.*;
+import com.w3resource.BasicExercisesPart_1.*;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //Scanner input = new Scanner(System.in);
-       // Scanner input2 = new Scanner(System.in);
-        //Ex5 exercise  = new Ex5();
+        Ex6 exercise  = new Ex6();
 
-        // int num1 = input.nextInt();
-        // int num2 = input2.nextInt();
-        // int sum = num1 * num2;
-        
-//        System.out.println("%d x %d = %d", num1, num2, sum);
-        System.out.println(2 * 2);
+        exercise.arithmetic();
 
 
     }
