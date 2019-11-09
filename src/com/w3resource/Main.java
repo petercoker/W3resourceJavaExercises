5fe96162802e323190e3a33c23d6a566529c0a6c
peter <petercoker@protonmail.com>
2019-11-09 11:39:53 +0000

    completed ex11

--- src/com/w3resource/Main.java
+++ src/com/w3resource/Main.java
@@ -4,11 +4,11 @@ import com.w3resource.BasicExercisesPart_1.*;
 
 
 public class Main {
-
     public static void main(String[] args) {
-        Ex10 exercise  = new Ex10();
+        Ex11 exercise  = new Ex11();
 
-        exercise.Cal();
+        System.out.println(exercise.GetArea(7.5));
+        System.out.println(exercise.GetPerimeter(7.5));
 
 
     }
