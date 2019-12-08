4308a02b6b0071921ced646ef5075e461c45859e
peter <petercoker@protonmail.com>
2019-12-08 22:39:11 +0000

    completed ex16

--- src/com/w3resource/BasicExercisesPart_1/Ex16.java
+++ src/com/w3resource/BasicExercisesPart_1/Ex16.java
@@ -2,5 +2,19 @@ package com.w3resource.BasicExercisesPart_1;
 
 public class Ex16{
 
+    public void PrintFace(){
 
+        String [] face = new String[5];
+
+        face[0] = " +\"\"\"\"\"+ ";
+        face[1] = "[| o o |]";
+        face[2] = " |  ^  |";
+        face[3] = " | '-' |";
+        face[4] = " +-----+";
+
+        for (String part : face) {
+            System.out.println(part);
+        }
+       
+    }
 }
--- src/com/w3resource/Main.java
+++ src/com/w3resource/Main.java
@@ -8,9 +8,9 @@ public class Main {
     //static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Ex15 exercise  = new Ex15();
+        Ex16 exercise  = new Ex16();
 
-        exercise.SwapTwoInteger();
+        exercise.PrintFace();
         
 
 
